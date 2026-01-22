a9f637ba7977934b4f9b5f6882d89d95659c2455
danpsh <danielpshonyak@gmail.com>
2026-01-22 14:43:16 -0600

    Fix formatting of title in app.py

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import streamlit as st
 
-st.title("🚴 2026 Fantasy Cycling")
+st.title("2026 Fantasy Cycling")
 st.write("Welcome to our draft league! Points system is ready.")
 
 # Just a test to see if it works
 st.info("Currently waiting for data...")
+
 
