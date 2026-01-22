3a6d5670e4d764e9a2f025d3d8b305694b54caeb
danpsh <danielpshonyak@gmail.com>
2026-01-22 14:42:58 -0600

    Update title and greeting for cycling app

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import streamlit as st
 
-st.title("🚴 2026 World Tour Tracker")
+st.title("🚴 2026 Fantasy Cycling")
 st.write("Welcome to our draft league! Points system is ready.")
 
 # Just a test to see if it works
 st.info("Currently waiting for data...")
+
